f74bf0caff4193357ef961dc42aba3203a7c9fff
vaibhav9852 <vaibhav.s@webkorps.com>
2024-11-22 22:49:33 +0530

    server ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, {Application , Request, Response , NextFunction} from "express"
+import cors from 'cors'
 import {router as userRoute} from "./routes/users.route"
 
 const app : Application = express()
 
+app.use(cors({origin : '*'}))
 app.use('/users',userRoute)
 
 app.get('/',(req:Request,res:Response,next:NextFunction) : void=>{
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import cors from 'cors'
+
 import app from "./app"
 
 const PORT : Number = 5005
 
-app.use(cors({origin : '*'}))
+
 
 app.listen(PORT,()=>{
     console.log('server started 5005')
